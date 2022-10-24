36fc32287929dcc8640662733f8c8fa3a9f62334
ZeptoBook <zeptobookusa@gmail.com>
2022-10-24 10:01:48 +0530

    Added list icons on navigation bar

--- MyList/ListController.swift
+++ MyList/ListController.swift
@@ -5,6 +5,14 @@ class ListController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        title = "List"
+        configureBarItems()
+    }
+    
+    private func configureBarItems() {
+        navigationItem.rightBarButtonItems = [
+            UIBarButtonItem(image: UIImage(systemName: "list.dash"), style: .done, target: self, action: nil),
+            //UIBarButtonItem(barButtonSystemItem: .add, target: self, action: nil)
+            UIBarButtonItem(image: UIImage(systemName: "folder.badge.plus"), style: .done, target: self, action: nil)
+            ]
     }
 }
