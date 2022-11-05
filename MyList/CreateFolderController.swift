509a5e95450dc5852237103965b5f7fa4c1265ea
ZeptoBook <zeptobookusa@gmail.com>
2022-11-05 17:31:13 +0530

    image buttons replaced with buttons

--- MyList/CreateFolderController.swift
+++ MyList/CreateFolderController.swift
@@ -14,49 +14,44 @@ class CreateFolderController: UIViewController, UITextFieldDelegate {
     
     //weak var delegate: FormControllerDelegate?
     var tfGroupName = UITextField()
-    var reminderBtn = UIButton(type: .system)
-    var calendarBtn = UIButton(type: .system)
-    var favoriteBtn = UIButton(type: .system)
-    var stackView = UIStackView()
     
-//    private var reminderBtn: UIButton = {
-//        //let btn = UIBarButtonItem(image: UIImage(systemName: "folder.badge.plus"), style: .done, target: CreateFolderController.self, action: nil)
-//        let btn =  UIButton()
-//        btn.setImage(UIImage(named: "folder.badge.plus"), for: .normal)
-//        btn.translatesAutoresizingMaskIntoConstraints = false
-//        return btn
-//    }()
+    var config = UIImage.SymbolConfiguration(pointSize: 28, weight: .regular, scale: .small)
     
-//    private var calendarBtn: UIButton = {
-//        //let btn = UIBarButtonItem(image: UIImage(systemName: "folder.badge.plus"), style: .done, target: CreateFolderController.self, action: nil)
-//        let btn = UIButton()
-//        btn.setImage(UIImage(named: "folder.badge.plus"), for: .normal)
-//        btn.translatesAutoresizingMaskIntoConstraints = false
-//        return btn
-//    }()
+    lazy var reminderBtn: UIButton = {
+        let btn = UIButton()
+        btn.setImage(UIImage(systemName: "bell", withConfiguration: config), for: .normal)
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        return btn
+    }()
     
-//    private var favoriteBtn: UIButton = {
-//        //let btn = UIBarButtonItem(image: UIImage(systemName: "folder.badge.plus"), style: .done, target: CreateFolderController.self, action: nil)
-//        let btn = UIButton()
-//        btn.setImage(UIImage(named: "folder.badge.plus"), for: .normal)
-//        btn.translatesAutoresizingMaskIntoConstraints = false
-//        return btn
-//    }()
+    lazy var calendarBtn: UIButton = {
+        let btn = UIButton()
+        btn.setImage(UIImage(systemName: "calendar", withConfiguration: config), for: .normal)
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        return btn
+    }()
     
-//    lazy var stackView: UIStackView = {
-//        let stack = UIStackView()
-//        stack.axis = .horizontal
-//        stack.spacing = 20.0
-//        stack.alignment = .fill
-//        stack.distribution = .equalSpacing
-//
-//
-//        stack.addArrangedSubview(self.reminderBtn)
-//        stack.addArrangedSubview(self.calendarBtn)
-//        stack.addArrangedSubview(self.favoriteBtn)
-//
-//        return stack
-//    }()
+    lazy var favoriteBtn: UIButton = {
+        let btn = UIButton()
+        btn.setImage(UIImage(systemName: "star", withConfiguration: config), for: .normal)
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        return btn
+    }()
+    
+    // Stack View
+    lazy var stackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.spacing = 20.0
+        stack.alignment = .fill
+        stack.distribution = .fillEqually
+        
+        stack.addArrangedSubview(self.reminderBtn)
+        stack.addArrangedSubview(self.calendarBtn)
+        stack.addArrangedSubview(self.favoriteBtn)
+        
+        return stack
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -64,7 +59,22 @@ class CreateFolderController: UIViewController, UITextFieldDelegate {
         view.backgroundColor = .systemBackground
         self.tfGroupName.delegate = self
         setupForm()
-        setupButtons()
+        //setupButtons()
+        
+        setImageAndConstraints()
+        view.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func setImageAndConstraints() {
+        self.view.addSubview(reminderBtn)
+        self.view.addSubview(calendarBtn)
+        self.view.addSubview(favoriteBtn)
+//        someImageView.widthAnchor.constraint(equalToConstant: 50).isActive = true
+//        someImageView.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        reminderBtn.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        reminderBtn.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 28).isActive = true
     }
     
     override func viewWillLayoutSubviews() {
@@ -101,76 +111,6 @@ class CreateFolderController: UIViewController, UITextFieldDelegate {
             tfGroupName.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
             tfGroupName.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8)])
         
-    }
-    
-    private func setupButtons() {
-//        reminderBtn.setImage(UIImage(named: "folder.badge.plus"), for: .normal)
-//        calendarBtn.setImage(UIImage(named: "folder.badge.plus"), for: .normal)
-//        favoriteBtn.setImage(UIImage(named: "folder.badge.plus"), for: .normal)
-        
-        // Reminder Button
-        reminderBtn.backgroundColor = .blue
-        reminderBtn.setTitle("Rem", for: .normal)
-        reminderBtn.tintColor = .white
-        reminderBtn.layer.cornerRadius = 5
-        reminderBtn.clipsToBounds = true
-        reminderBtn.translatesAutoresizingMaskIntoConstraints = false
-        
-        
-        // Calendar Button
-        calendarBtn.backgroundColor = .blue
-        calendarBtn.setTitle("Cal", for: .normal)
-        calendarBtn.tintColor = .white
-        calendarBtn.layer.cornerRadius = 5
-        calendarBtn.clipsToBounds = true
-        calendarBtn.translatesAutoresizingMaskIntoConstraints = false
-        
-        
-        // Favorite Button
-        favoriteBtn.backgroundColor = .blue
-        favoriteBtn.setTitle("Fav", for: .normal)
-        favoriteBtn.tintColor = .white
-        favoriteBtn.layer.cornerRadius = 5
-        favoriteBtn.clipsToBounds = true
-        favoriteBtn.translatesAutoresizingMaskIntoConstraints = false
-        
-        
-        view.addSubview(reminderBtn)
-        view.addSubview(calendarBtn)
-        view.addSubview(favoriteBtn)
-        NSLayoutConstraint.activate([
-            reminderBtn.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
-            reminderBtn.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
-            reminderBtn.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8)])
-        
-        NSLayoutConstraint.activate([
-            calendarBtn.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
-            calendarBtn.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
-            calendarBtn.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8)])
-        
-        NSLayoutConstraint.activate([
-            favoriteBtn.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
-            favoriteBtn.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 60),
-            favoriteBtn.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8)])
-        
-        view.translatesAutoresizingMaskIntoConstraints = false
-        
-        //Stack View
-//        stackView.axis = .horizontal
-//        stackView.spacing = 20.0
-//        stackView.alignment = .fill
-//        stackView.distribution = .equalSpacing
-//        stackView.addArrangedSubview(reminderBtn)
-//        stackView.addArrangedSubview(calendarBtn)
-//        stackView.addArrangedSubview(favoriteBtn)
-//        view.addSubview(stackView)
-//        stackView.translatesAutoresizingMaskIntoConstraints = false
-//
-//        NSLayoutConstraint.activate([
-//            stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
-//            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-//            stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-//            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor)])
     }
     
     @objc private func handleNewText() {
